be0433d542d7a44c40e4d7dadc68ca38b8bdcc73
rdhall12 <richard.hall2@imail.org>
2022-03-03 16:12:11 -0700

    Completed module

--- js/sight.js
+++ js/sight.js
@@ -1,35 +1,33 @@
 class SVGElement {
-  const type;
-  const namespace;
   constructor(type) {
     this.type = type;
     this.namespace = 'http://www.w3.org/2000/svg';
-    this.node = document.createElementNS(namespace, type);
-
+    this.node = document.createElementNS(this.namespace, this.type);
     return this;
   }
 
-  attr(attrs: object) {
-    for( const [key, value] of attrs.entries()) {
-      this.node.setAttributeNS(null, value);
+  attr(attrs) {
+    for (const [key, value] of Object.entries(attrs)) {
+      this.node.setAttributeNS(null, key, value);
     }
     return this;
   }
 
   append(element) {
-    parent = typeof(element) === string ? document.querySelector(element) : element.node;
-    this.node.append(parent);
-
+    const parent = (typeof element === 'string') ? document.querySelector(element) : element.node;
+    parent.appendChild(this.node);
     return this;
   }
 }
 
 class Sight {
-
   constructor(selector, width, height) {
-    this.svg = new SVGElement(this.svg).attr({viewbox: `0 0 ${width} ${heigth}`}).append(selector);
+    this.svg = new SVGElement('svg').attr({
+      viewbox: `0 0 ${width} ${height}`,
+    }).append(selector);
   }
 
-  draw(type, attrs).attr(attrs).append(svg);
-
+  draw(type, attrs) {
+    return new SVGElement(type).attr(attrs).append(this.svg);
+  }
 }
